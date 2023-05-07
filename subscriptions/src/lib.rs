078090c60a3c49d7ca1c8e4b4df0c520c973443d
CosminPerRam <cosmin.p@live.com>
2023-05-08 02:14:30 +0300

    [] Test first prototype implementation

--- subscriptions/src/lib.rs
+++ subscriptions/src/lib.rs
@@ -1,111 +1,61 @@
 use std::pin::Pin;
-use std::time::Duration;
-use juniper::{GraphQLEnum, graphql_object, graphql_subscription, graphql_value, FieldError};
+use juniper::{graphql_object, graphql_subscription, FieldError};
 use futures::Stream;
 use async_stream::stream;
 use context::Context;
-use tokio;
+use gamedig::valve_master_server::{query_singular, Region, SearchFilters, Filter};
+use gamedig::protocols::valve::{Engine, query, GatheringSettings};
 
-#[derive(Clone, Copy, GraphQLEnum)]
-enum UserKind {
-    Admin,
-    User,
-    Guest,
-}
-
-struct User {
-    id: i32,
-    kind: UserKind,
-    name: String,
+struct Server {
+    name: String
 }
 
 // Field resolvers implementation
 #[graphql_object(context = Context)]
-impl User {
-    fn id(&self) -> i32 {
-        self.id
-    }
-
-    fn kind(&self) -> UserKind {
-        self.kind
-    }
-
+impl Server {
     fn name(&self) -> &str {
         &self.name
     }
-
-    async fn friends(&self) -> Vec<User> {
-        if self.id == 1 {
-            vec![
-                User {
-                    id: 11,
-                    kind: UserKind::User,
-                    name: "user11".into(),
-                },
-                User {
-                    id: 12,
-                    kind: UserKind::Admin,
-                    name: "user12".into(),
-                },
-                User {
-                    id: 13,
-                    kind: UserKind::Guest,
-                    name: "user13".into(),
-                },
-            ]
-        } else if self.id == 2 {
-            vec![User {
-                id: 21,
-                kind: UserKind::User,
-                name: "user21".into(),
-            }]
-        } else if self.id == 3 {
-            vec![
-                User {
-                    id: 31,
-                    kind: UserKind::User,
-                    name: "user31".into(),
-                },
-                User {
-                    id: 32,
-                    kind: UserKind::Guest,
-                    name: "user32".into(),
-                },
-            ]
-        } else {
-            vec![]
-        }
-    }
 }
 
-type UsersStream = Pin<Box<dyn Stream<Item = Result<User, FieldError>> + Send>>;
+type ServersStream = Pin<Box<dyn Stream<Item = Result<Server, FieldError>> + Send>>;
 
 pub struct Subscription;
 
 #[graphql_subscription(context = Context)]
 impl Subscription {
-    async fn users() -> UsersStream {
-        let mut counter = 0;
-        let mut interval = tokio::time::interval(Duration::from_millis(300));
+    async fn servers() -> ServersStream {
         let stream = stream! {
             loop {
-                counter += 1;
-                interval.tick().await;
-                println!("{counter}");
-                if counter == 4 {
-                    yield Err(FieldError::new(
-                        "some field error from handler",
-                        graphql_value!("some additional string"),
-                    ))
-                } else if counter < 4 {
-                    yield Ok(User {
-                        id: counter,
-                        kind: UserKind::Admin,
-                        name: "stream user".into(),
+                // see warp_subscriptions example from juniper
+
+                let search_filters = SearchFilters::new()
+                    .insert(Filter::RunsAppID(440))
+                    .insert(Filter::CanBeEmpty(false))
+                    .insert(Filter::CanBeFull(false))
+                    .insert(Filter::CanHavePassword(false))
+                    .insert(Filter::IsSecured(true))
+                    .insert(Filter::HasTags(&["minecraft"]));
+
+                let responses = query_singular(Region::Europe, Some(search_filters)).unwrap();
+
+                for server in responses {
+                    let ip = server.0.to_string();
+                    let port = server.1;
+                    println!("{ip}:{port}");
+                    let gather_settings = GatheringSettings {
+                        players: false,
+                        rules: false
+                    };
+
+                    let response = query(&ip, port, Engine::Source(None), Some(gather_settings), None).unwrap();
+
+                    yield Ok(Server {
+                        name: response.info.name
                     })
-                } else {
-                    return;
                 }
+
+                return
             }
         };
 
