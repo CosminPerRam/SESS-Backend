5a64724098030008ae7e7ef64ec42f870c2eab0c
CosminPerRam <cosmin.p@live.com>
2023-05-09 00:56:03 +0300

    [] Add other valve query fields to player and server

--- subscriptions/src/player.rs
+++ subscriptions/src/player.rs
@@ -3,13 +3,17 @@ use juniper::GraphQLObject;
 
 #[derive(GraphQLObject)]
 pub struct Player {
-    name: String
+    name: String,
+    score: i32,
+    duration: f64
 }
 
 impl Player {
-    pub fn from_valve_response(player: &ServerPlayer) -> Self {
+    pub fn from_valve_response(player: ServerPlayer) -> Self {
         Self {
-            name: player.clone().name
+            name: player.name,
+            score: player.score as i32,
+            duration: player.duration as f64
         }
     }
 }
--- subscriptions/src/server.rs
+++ subscriptions/src/server.rs
@@ -4,18 +4,46 @@ use crate::player::Player;
 
 #[derive(GraphQLObject)]
 pub struct Server {
+    protocol: i32,
     name: String,
+    map: String,
+    folder: String,
+    game: String,
+    appid: i32,
+    players_online: i32,
+    players_maximum: i32,
+    players_bots: i32,
+    //server_type: Server,
+    //environment_type: Environment,
+    has_password: bool,
+    vac_secured: bool,
+    version: String,
+    //extra_data: Option<ExtraData>,
+    is_mod: bool,
+    //mod_data: Option<ModData>,
     players: Vec<Player>
 }
 
 impl Server {
     pub fn from_valve_response(response: Response) -> Self {
         Self {
+            protocol: response.info.protocol as i32,
             name: response.info.name,
+            map: response.info.map,
+            folder: response.info.folder,
+            game: response.info.game,
+            appid: response.info.appid as i32,
+            players_online: response.info.players_online as i32,
+            players_maximum: response.info.players_maximum as i32,
+            players_bots: response.info.players_bots as i32,
+            has_password: response.info.has_password,
+            vac_secured: response.info.vac_secured,
+            version: response.info.version,
             players: response.players.unwrap_or(Vec::new())
-                .iter()
+                .into_iter()
                 .map(|p| Player::from_valve_response(p))
-                .collect()
+                .collect(),
+            is_mod: response.info.is_mod,
         }
     }
 }
