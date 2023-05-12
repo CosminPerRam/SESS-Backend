8a5e9146405612f41babb6f2b122e71e463c389a
CosminPerRam <cosmin.p@live.com>
2023-05-12 23:16:37 +0300

    [] Fix context being created each time

--- context/src/lib.rs
+++ context/src/lib.rs
@@ -1,14 +1,9 @@
+use once_cell::sync::Lazy;
+use std::sync::Arc;
 use tokio::sync::RwLock;
 use statistics::Statistics;
 
-#[derive(Clone)]
-pub struct Database {
-    pub statistics: Statistics
-}
-
-pub struct DatabaseContext(pub RwLock<Database>);
-
-pub fn get_context() -> DatabaseContext {
+static DATA: Lazy<DatabaseContext> = Lazy::new(|| {
     let stats = Statistics {
         queries: 0
     };
@@ -17,7 +12,18 @@ pub fn get_context() -> DatabaseContext {
         statistics: stats
     };
 
-    DatabaseContext(RwLock::new(database))
+    DatabaseContext(Arc::new(RwLock::new(database)))
+});
+
+#[derive(Clone)]
+pub struct Database {
+    pub statistics: Statistics
+}
+
+pub struct DatabaseContext(pub Arc<RwLock<Database>>);
+
+pub fn get_context() -> DatabaseContext {
+    DatabaseContext(DATA.0.clone())
 }
 
 impl juniper::Context for DatabaseContext {}
