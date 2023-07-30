b13b21ea2509b4f1ded7e76d1e0bdf0301ecc41a
CosminPerRam <cosmin.p@live.com>
2023-07-30 20:27:36 +0300

    Add postgress in docker compose

---
+++ db/src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn db_do() {
+    println!("test");
+}
--- presentation/src/main.rs
+++ presentation/src/main.rs
@@ -2,12 +2,16 @@ mod setup_log;
 mod start_warp;
 mod schema;
 
+use db::db_do;
 use crate::setup_log::setup_log;
 use crate::start_warp::start_warp;
+
 
 #[tokio::main]
 async fn main() {
     setup_log();
 
-    start_warp().await;
+    db_do();
+
+    //start_warp().await;
 }
